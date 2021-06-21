2d89568a296024050a289e6526b7caa1b05baed5
christkz <kezakimanachrist2@gmail.com>
2021-06-21 10:59:37 +0200

    add Home page

---
+++ src/pages/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Navigation from '../components/Navigation';
+import Logo from '../components/Logo';
+
+const Home = () => {
+    return (
+        <div className="App">
+
+            <Navigation />
+            <Logo />
+            <h1>Accuell</h1>
+
+        </div>
+    );
+}
+export default Home;
