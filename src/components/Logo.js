52337dfe22a1c7d1b7786ea3e70b24b2062844b8
christkz <kezakimanachrist2@gmail.com>
2021-06-21 11:02:24 +0200

    add Logo.js for component Logo

---
+++ src/components/Logo.js
@@ -0,0 +1,11 @@
+import React from 'react';
+const Logo = () => {
+    return (
+        <div className="logo">
+            <img src="./img/logo.png" alt="logo" />
+            <h3>React World</h3>
+        </div>
+    );
+};
+
+export default Logo;
