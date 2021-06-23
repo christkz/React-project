131fec61881a3f2a9f843e2abe3bd9a5f44ba82f
christkz <kezakimanachrist2@gmail.com>
2021-06-23 16:04:55 +0200

    use effect hook and state

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -1,14 +1,28 @@
-import React,{useState} from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from "axios";
+import Card from './Card';
 
 const Countries = () => {
-     const [data, setData] = useState([]);
-     axios.get("https://restcountries.eu/rest/v2/all?fields=name;population;region;capital;flag")
-     .then((res)=>setData(res.data));
-     
+    const [data, setData] = useState([]);
+
+
+    useEffect(() => {
+
+        axios.get("https://restcountries.eu/rest/v2/all?fields=name;population;region;capital;flag")
+            .then((res) => setData(res.data));
+
+        console.log(data);
+    }, []);
+
     return (
-        <div>
-       
+        <div className="countries">
+            <ul className="countries-list">
+                {data.map((country) =>
+                (
+                   <Card country ={country} key = {country.name} />
+
+                ))}
+            </ul>
         </div>
     );
 };
