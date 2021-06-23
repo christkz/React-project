72be153cd3e124ef47786cdca33036daaa84606c
christkz <kezakimanachrist2@gmail.com>
2021-06-23 16:05:26 +0200

    add Card.js component

---
+++ src/components/Card.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Card = (propos) => {
+    
+console.log(propos);
+
+    return (
+        <div>
+            card
+        </div>
+    );
+};
+
+export default Card;
