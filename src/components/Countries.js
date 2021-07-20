05b029c7ccbfac78e13904816f593a0ed930bba4
christkz <kezakimanachrist2@gmail.com>
2021-07-20 22:50:09 +0200

    import  card component and charge data for all countries

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -4,22 +4,41 @@ import Card from './Card';
 
 const Countries = () => {
     const [data, setData] = useState([]);
-
+    const [sortedData, setSortedData] = useState([]);
+    const [playOnce, setPlayOnce] = useState(true);
+    const [rangeValue, setRangeValue] = useState(40);
 
     useEffect(() => {
 
-        axios.get("https://restcountries.eu/rest/v2/all?fields=name;population;region;capital;flag")
-            .then((res) => setData(res.data));
+        if (playOnce) {
 
-        console.log(data);
-    }, []);
+            axios.get("https://restcountries.eu/rest/v2/all?fields=name;population;region;capital;flag")
+                .then((res) => {
+                    setData(res.data);
+                    setPlayOnce(false);
+                });
+
+        }
+        const sortedCountry = () => {
+            const countryObj = Object.keys(data).map((i) => data[i]);
+
+            const sortedArray = countryObj.sort((a, b) => {
+
+                return b.population - a.population;
+            });
+            sortedArray.lenght = rangeValue;
+            setSortedData(sortedArray);
+        };
+           sortedCountry();
+       },
+        [data,rangeValue,playOnce]);
 
     return (
         <div className="countries">
             <ul className="countries-list">
-                {data.map((country) =>
+                {sortedData.map((country) =>
                 (
-                   <Card country ={country} key = {country.name} />
+                    <Card country={country} key={country.name} />
 
                 ))}
             </ul>
