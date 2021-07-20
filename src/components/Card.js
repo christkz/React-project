845244038f4298ad434db0994e1567d1a8dcec4c
christkz <kezakimanachrist2@gmail.com>
2021-07-20 22:52:17 +0200

    passed country in props and display name, capital,population

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,13 +1,26 @@
 import React from 'react';
 
-const Card = (propos) => {
-    
-console.log(propos);
+const Card = (props) => {
+ const {country} = props;    
+
+ const numberFormat= (x) =>{
+
+    return x.toString().replace(/\B(?=(\d{3}) +(?!\d))/g," ");
+ }
 
     return (
-        <div>
-            card
+        
+        <li className="card">
+        <img src={country.flag} alt="flag" />
+        <div className="data-container">
+        <ul>
+        <li>{country.name}</li>
+        <li>{country.capital}</li>
+        <li>pop.{numberFormat(country.population)}</li>
+        </ul>
+
         </div>
+        </li>
     );
 };
 
